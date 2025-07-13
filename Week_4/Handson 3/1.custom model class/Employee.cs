547a5cc2a22ce28222bd07f48506169ec4597cb4
Naveenpro123 <official.naveen2005@gmail.com>
2025-07-13 12:17:33 +0530

    Create Employee.cs

---
+++ Week_4/Handson 3/1.custom model class/Employee.cs
@@ -0,0 +1,19 @@
+namespace FirstWebApi.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Salary { get; set; }
+
+        public bool Permanent { get; set; }
+
+        public Department Department { get; set; }
+
+        public List<Skill> Skills { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
