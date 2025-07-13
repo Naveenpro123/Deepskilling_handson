fe3143f4406e7dc195f5610732825a1a9a13ca17
Naveenpro123 <official.naveen2005@gmail.com>
2025-07-13 19:55:39 +0530

    Create Jsonwebtoken.cs

---
+++ Week_4/Handson 5/Main/Jsonwebtoken.cs
@@ -0,0 +1,30 @@
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
+
+string securityKey = "mysuperdupersecret";
+var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysuperdupersecuresecretkey@123456"));
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "mySystem",
+        ValidAudience = "myUsers",
+        IssuerSigningKey = key
+    };
+});
