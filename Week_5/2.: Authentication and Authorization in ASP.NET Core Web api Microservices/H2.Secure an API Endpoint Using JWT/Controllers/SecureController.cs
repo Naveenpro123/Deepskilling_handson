b93e0864d268b5b18f7d3213cdbfa01ecb65cc5e
Naveenpro123 <official.naveen2005@gmail.com>
2025-07-20 19:34:41 +0530

    Create SecureController.cs

---
+++ Week_5/2.: Authentication and Authorization in ASP.NET Core Web api Microservices/H2.Secure an API Endpoint Using JWT/Controllers/SecureController.cs
@@ -0,0 +1,16 @@
+//EndPoint Controller
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class SecureController : ControllerBase
+{
+ [HttpGet("data")]
+ [Authorize]
+ public IActionResult GetSecureData()
+ {
+ return Ok("This is protected data only visible with a valid JWT token.");
+ }
+}
