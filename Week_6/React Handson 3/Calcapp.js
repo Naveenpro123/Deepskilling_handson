0d21f63dabf0abe81eac4d02c34ed8e86a39d380
Naveenpro123 <official.naveen2005@gmail.com>
2025-07-27 18:21:57 +0530

    Create Calcapp.js

---
+++ Week_6/React Handson 3/Calcapp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Calc} from './Calc.js';
+function Calcapp() {
+    return (
+        <Calc Name={"Mohamed Gows Z"} School={"Anne Marie Matric School"}
+            total={528} goal={6} />
+    );
+}
+export default Calcapp;
