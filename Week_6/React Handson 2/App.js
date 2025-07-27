60a30915b654e89ca4cf1978c45831aeb2c0410a
Naveenpro123 <official.naveen2005@gmail.com>
2025-07-27 18:20:13 +0530

    Create App.js

---
+++ Week_6/React Handson 2/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Component/Home';
+import About from './Component/About';
+import Contact from './Component/Contact';
+
+function App() {
+    return (
+        <div className="center-content">
+            <Home />
+            <About />
+            <Contact />
+            
+        </div>
+     
+  );
+}
+
+export default App;
