f5b952e4ead5262d6abb82bebf52262abdb42e29
Naveenpro123 <official.naveen2005@gmail.com>
2025-08-03 21:55:07 +0530

    Create App.js

---
+++ Week_7/React Handson 9 cricketapp/App.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import ListofPlayers from './cricketapp/ListofPlayers';
+import IndianPlayers from './cricketapp/IndianPlayers';
+
+function App(){
+  const [flag, setFlag] = useState(true);
+  return( <div >
+    <button onClick={() => (setFlag(!flag))}>Toggle View</button>
+    {flag ? <ListofPlayers /> : <IndianPlayers />} }
+);
+</div>
+}
+export default App;
