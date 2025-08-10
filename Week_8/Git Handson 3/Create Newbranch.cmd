50fc7f010fc9bf6d8a64ab864fd93e4a17a3cc2c
Naveenpro123 <official.naveen2005@gmail.com>
2025-08-10 20:34:29 +0530

    Create Create Newbranch.cmd

---
+++ Week_8/Git Handson 3/Create Newbranch.cmd
@@ -0,0 +1,8 @@
+1.Create and Switch to new branch:
+$ git pull --rebase origin master
+
+2.Add a File and Commit It
+$ echo "This is from GitNewBranch" > branchfile.txt
+$ git add branchfile.txt
+$ git commit -m "Add branchfile.txt from GitNewBranch"
+
