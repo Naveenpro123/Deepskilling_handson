dc3ed1304e56993ce0aa362d7a64ca6a48f48edc
Naveenpro123 <official.naveen2005@gmail.com>
2025-08-10 20:27:37 +0530

    Create Git project.cmd

---
+++ Week_8/Git Handson 1/Git project.cmd
@@ -0,0 +1,26 @@
+1.Create a new project folder:
+$ mkdir GitDemo
+$ cd GitDemo
+
+2.Initialize Git repo:
+$ git init
+
+3.Create a file:
+$ echo "Welcome to Git hands-on lab!" > welcome.txt
+
+4.Verify file creation:
+$ ls
+>welcome.txt
+
+5. View file content:
+$ cat welcome.txt
+
+6.Check Git status
+$ git status
+
+7.Add file to Git staging:
+$ git add welcome.txt
+
+8.Commit:
+$  git commit
+
