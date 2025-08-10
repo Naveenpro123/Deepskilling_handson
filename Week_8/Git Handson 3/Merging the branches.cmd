1e711824062d59e833444bad79a3a3705922d6ad
Naveenpro123 <official.naveen2005@gmail.com>
2025-08-10 20:34:47 +0530

    Create Merging the branches.cmd

---
+++ Week_8/Git Handson 3/Merging the branches.cmd
@@ -0,0 +1,12 @@
+1.Switch Back to Master Branch:
+$ git checkout master
+
+2. Merge the New Branch into Master:
+$ git merge GitNewBranch
+
+//Vs code get opened with merge text
+close it
+
+3.Add and Commit:
+$ git add .
+$ git commit -m "Resolved merge conflict and merged GitNewBranch"
