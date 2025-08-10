3bb51e2bd674a5e0aff088e2045576d09e21f5b3
Naveenpro123 <official.naveen2005@gmail.com>
2025-08-10 20:33:08 +0530

    Create Link to GitHub repo.cmd

---
+++ Week_8/Git Handson 2/Link to GitHub repo.cmd
@@ -0,0 +1,8 @@
+1.add origin:
+$ git remote add origin https://github.com/MOHAMEDGOWS/GitDemo.git
+
+2.Pull remote code first (since repo already exists)
+$ git pull --rebase origin master
+
+3.Push to GitHub
+$ git push -u origin master
