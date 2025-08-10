bc0d1fc0b2b9d9cb270170f883002dcd7e4207ae
Naveenpro123 <official.naveen2005@gmail.com>
2025-08-10 20:32:34 +0530

    Create Creating logfile.cmd

---
+++ Week_8/Git Handson 2/Creating logfile.cmd
@@ -0,0 +1,14 @@
+1.Create a local project folder
+$ mkdir MyGitProject
+$ cd MyGitProject
+$ git init
+
+2.Create .logfile and log/folder
+$ echo "This is a log file" > example.log
+$ mkdir log
+$ echo "Log data" > log/app.log
+
+3.Verify
+$ ls
+$ ls.log
+
