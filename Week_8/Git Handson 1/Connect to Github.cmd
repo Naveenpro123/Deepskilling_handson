8502b8bb3c63b6fc699a97b6f6e91349eb21ea72
Naveenpro123 <official.naveen2005@gmail.com>
2025-08-10 20:28:18 +0530

    Create Connect to Github.cmd

---
+++ Week_8/Git Handson 1/Connect to Github.cmd
@@ -0,0 +1,8 @@
+1.Add remote origin:
+$ git remote add origin https://github.com/MOHAMEDGOWS/GitDemo.git
+
+2.Pull from Github:
+$ git pull origin master
+
+3.Push local repo to Github:
+$ git push -u origin master
